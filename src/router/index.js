a5756065b62b29ef867ded8097a71ac2dba2c779
海力森 <halrison@outlook.com>
2023-11-27 02:07:43 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import FrontLayout from '../layouts/FrontLayout.vue'
+import RearLayout from '../layouts/RearLayout.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
